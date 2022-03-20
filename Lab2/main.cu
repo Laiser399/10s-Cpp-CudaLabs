d1faee1b8966daed162260c91c1990ea2e0d8b06
Sergestus Shimonov <Laiser399@mail.ru>
2022-03-21 01:14:51 +0300

    Lab2: texture reference replaced with texture object + size2D

--- Lab2/main.cu
+++ Lab2/main.cu
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <fstream>
 #include <cstdio>
 
 using std::fstream;
@@ -18,88 +17,114 @@ using std::endl;
         }                                                                       \
     }
 
-texture<uchar4, 2, cudaReadModeElementType> tex;
+struct size2D {
+    int width, height;
 
-__global__ void kernel(uchar4 *result, int sourceWidth, int sourceHeight, int width, int height) {
+    int getSize() const {
+        return width * height;
+    }
+};
+
+__global__ void kernel(cudaTextureObject_t source, uchar4 *target, size2D sourceSize, size2D targetSize) {
     auto totalThreadsCountX = gridDim.x * blockDim.x;
     auto totalThreadsCountY = gridDim.y * blockDim.y;
     auto threadIdX = blockDim.x * blockIdx.x + threadIdx.x;
     auto threadIdY = blockDim.y * blockIdx.y + threadIdx.y;
 
-    auto compressionCoefficientX = sourceWidth / width;
-    auto compressionCoefficientY = sourceHeight / height;
+    auto compressionCoefficientX = sourceSize.width / targetSize.width;
+    auto compressionCoefficientY = sourceSize.height / targetSize.height;
 
-    for (unsigned int i = threadIdX; i < width; i += totalThreadsCountX) {
-        for (unsigned int j = threadIdY; j < height; j += totalThreadsCountY) {
-            auto a = tex2D(tex, compressionCoefficientX * i, compressionCoefficientY * j);
-            auto b = tex2D(tex, compressionCoefficientX * i + 1, compressionCoefficientY * j);
-            auto c = tex2D(tex, compressionCoefficientX * i, compressionCoefficientY * j + 1);
-            auto d = tex2D(tex, compressionCoefficientX * i + 1, compressionCoefficientY * j + 1);
+    for (unsigned int i = threadIdX; i < targetSize.width; i += totalThreadsCountX) {
+        for (unsigned int j = threadIdY; j < targetSize.height; j += totalThreadsCountY) {
+            auto a = tex2D<uchar4>(source, compressionCoefficientX * i, compressionCoefficientY * j);
+            auto b = tex2D<uchar4>(source, compressionCoefficientX * i + 1, compressionCoefficientY * j);
+            auto c = tex2D<uchar4>(source, compressionCoefficientX * i, compressionCoefficientY * j + 1);
+            auto d = tex2D<uchar4>(source, compressionCoefficientX * i + 1, compressionCoefficientY * j + 1);
             auto avgR = (a.x + b.x + c.x + d.x) / 4;
             auto avgG = (a.y + b.y + c.y + d.y) / 4;
             auto avgB = (a.z + b.z + c.z + d.z) / 4;
             auto avgA = (a.w + b.w + c.w + d.w) / 4;
-            result[j * width + i] = make_uchar4(avgR, avgG, avgB, avgA);
+            target[j * targetSize.width + i] = make_uchar4(avgR, avgG, avgB, avgA);
         }
     }
 }
 
+bool isValidSizes(const size2D &sourceSize, const size2D &targetSize) {
+    if (targetSize.width > sourceSize.width
+        || targetSize.height > sourceSize.height
+        || sourceSize.width % targetSize.width != 0
+        || sourceSize.height % targetSize.height != 0) {
+        return false;
+    }
+
+    return true;
+}
 
 int main() {
-    int targetWidth = 960, targetHeight = 540;
+    size2D targetSize{};
+    targetSize.width = 960;
+    targetSize.height = 540;
     auto sourceFilePath = "..\\image1_binary";
     auto targetFilePath = "..\\image1_binary_res";
 
-    int sourceWidth, sourceHeight;
+    size2D sourceSize{};
 
+    // read source
     FILE *file = fopen(sourceFilePath, "rb");
-    fread(&sourceWidth, sizeof(int), 1, file);
-    fread(&sourceHeight, sizeof(int), 1, file);
-    auto *data = new uchar4[sourceWidth * sourceHeight];
-    fread(data, sizeof(uchar4), sourceWidth * sourceHeight, file);
+    fread(&sourceSize.width, sizeof(int), 1, file);
+    fread(&sourceSize.height, sizeof(int), 1, file);
+    auto *data = new uchar4[sourceSize.getSize()];
+    fread(data, sizeof(uchar4), sourceSize.getSize(), file);
     fclose(file);
 
-    if (targetWidth > sourceWidth
-        || targetHeight > sourceHeight
-        || sourceWidth % targetWidth != 0
-        || sourceHeight % targetHeight != 0) {
+    if (!isValidSizes(sourceSize, targetSize)) {
         cout << "Error: wrong target image size." << endl;
         return 1;
     }
 
+    // move data to cuda memory
     cudaArray *cudaData;
-    auto channel = cudaCreateChannelDesc<uchar4>();
-    CSC(cudaMallocArray(&cudaData, &channel, sourceWidth, sourceHeight))
-//    CSC(cudaMemcpy2DToArray(cudaData, 0, 0, data,
-//                            sourceWidth * sizeof(uchar4), sourceWidth * sizeof(uchar4), sourceHeight,
-//                            cudaMemcpyHostToDevice))
-    CSC(cudaMemcpyToArray(cudaData, 0, 0, data, sizeof(uchar4) * sourceWidth * sourceHeight, cudaMemcpyHostToDevice))
+    size_t pitch;
+    CSC(cudaMallocPitch(&cudaData, &pitch, sourceSize.width * sizeof(uchar4), sourceSize.height))
+    CSC(cudaMemcpy2D(cudaData, pitch, data,
+                     sourceSize.width * sizeof(uchar4), sourceSize.width * sizeof(uchar4), sourceSize.height,
+                     cudaMemcpyHostToDevice))
     delete[] data;
 
-    tex.addressMode[0] = cudaAddressModeClamp;
-    tex.addressMode[1] = cudaAddressModeClamp;
-    tex.channelDesc = channel;
-    tex.filterMode = cudaFilterModePoint;
-    tex.normalized = false;
-    CSC(cudaBindTextureToArray(tex, cudaData, channel))
+    // creating texture
+    cudaTextureObject_t tex;
+    cudaResourceDesc resDesc{};
+    resDesc.resType = cudaResourceTypePitch2D;
+    resDesc.res.pitch2D.devPtr = cudaData;
+    resDesc.res.pitch2D.width = sourceSize.width;
+    resDesc.res.pitch2D.height = sourceSize.height;
+    resDesc.res.pitch2D.desc = cudaCreateChannelDesc<uchar4>();
+    resDesc.res.pitch2D.pitchInBytes = pitch;
+    cudaTextureDesc texDesc{};
+    CSC(cudaCreateTextureObject(&tex, &resDesc, &texDesc, nullptr))
 
+    // allocating memory for result
     uchar4 *cudaResult;
-    CSC(cudaMalloc(&cudaResult, sizeof(uchar4) * targetWidth * targetHeight))
+    CSC(cudaMalloc(&cudaResult, sizeof(uchar4) * targetSize.getSize()))
 
-    kernel<<<dim3(16, 16), dim3(32, 32)>>>(cudaResult, sourceWidth, sourceHeight, targetWidth, targetHeight);
+    // core
+    kernel<<<dim3(16, 16), dim3(32, 32)>>>(tex, cudaResult, sourceSize, targetSize);
     CSC(cudaDeviceSynchronize())
     CSC(cudaGetLastError())
 
-    auto *localResult = new uchar4[targetWidth * targetHeight];
-    CSC(cudaMemcpy(localResult, cudaResult, sizeof(localResult[0]) * targetWidth * targetHeight, cudaMemcpyDeviceToHost))
-    CSC(cudaFreeArray(cudaData))
+    // move result from device to host
+    auto *localResult = new uchar4[targetSize.getSize()];
+    CSC(cudaMemcpy(localResult, cudaResult, sizeof(localResult[0]) * targetSize.getSize(),
+                   cudaMemcpyDeviceToHost))
+    CSC(cudaDestroyTextureObject(tex))
+    CSC(cudaFree(cudaData))
     CSC(cudaFree(cudaResult))
-    CSC(cudaUnbindTexture(tex))
 
+    // save result
     FILE *outFile = fopen(targetFilePath, "wb");
-    fwrite(&targetWidth, 1, sizeof(targetWidth), outFile);
-    fwrite(&targetHeight, 1, sizeof(targetHeight), outFile);
-    fwrite(localResult, sizeof(localResult[0]), targetWidth * targetHeight, outFile);
+    fwrite(&targetSize.width, 1, sizeof(targetSize.height), outFile);
+    fwrite(&targetSize.height, 1, sizeof(targetSize.height), outFile);
+    fwrite(localResult, sizeof(localResult[0]), targetSize.getSize(), outFile);
     fclose(outFile);
 
     delete[] localResult;
