bba16d9edd15030d4445895be3f1de34a2867fd8
Sergey Semenov <Laiser399@mail.ru>
2022-04-30 19:31:25 +0300

    Lab4: read input matrix

--- Lab4/main.cu
+++ Lab4/main.cu
@@ -1,6 +1,57 @@
 #include <iostream>
+#include <tuple>
+#include <string>
+#include <fstream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+using std::cout;
+using std::cerr;
+using std::cin;
+using std::endl;
+using std::istream;
+using std::ifstream;
+using std::tuple;
+using std::string;
+
+
+tuple<int, float *> readInput(istream &input) {
+    int n;
+    input >> n;
+
+    auto *matrix = new float[n * n];
+
+    for (int i = 0; i < n * n; ++i) {
+        input >> matrix[i];
+    }
+
+    return tuple<int, float *>{n, matrix};
+}
+
+
+tuple<int, float *> readInput(int argc, char *argv[]) {
+    if (argc == 2) {
+        auto inputFilePath = argv[1];
+
+        ifstream input(inputFilePath);
+        if (!input.is_open()) {
+            cerr << "Could not open input file \"" << inputFilePath << "\"." << endl;
+            exit(1);
+        }
+
+        auto result = readInput(input);
+
+        input.close();
+
+        return result;
+    } else {
+        return readInput(cin);
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    int matrixSize;
+    float *matrix;
+    std::tie(matrixSize, matrix) = readInput(argc, argv);
+
     return 0;
 }
